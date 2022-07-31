1570ad0050ca900c899e6bbcdd69465189605c26
Italo Andrade <italoandrade13@gmail.com>
2022-07-30 22:36:25 -0300

    feat: add route login/validate

--- app/backend/src/middleware/loginDataValidate.ts
+++ app/backend/src/middleware/loginDataValidate.ts
@@ -1,8 +1,11 @@
 import Joi = require('joi');
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response, NextFunction } from 'express';
+import jwt = require('jsonwebtoken');
 
 class LoginDataValidate {
+  private jwtSecret = process.env.JWT_SECRET || 'jwt_secret';
+
   private joiValidate = (attributes: { password: string, email: string }) => {
     const { error } = Joi.object({
       password: Joi.string().min(6).required(),
@@ -22,6 +25,20 @@ class LoginDataValidate {
       return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ message: error.message });
     }
     return next();
+  };
+
+  public authToken = async (req: Request | any, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token not found' });
+
+    try {
+      const { role, email }: any = jwt.verify(token, this.jwtSecret);
+      req.userInfo = { role, email };
+      return next();
+    } catch (err) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Expired or invalid token' });
+    }
   };
 }
 
--- app/backend/src/routes/login.ts
+++ app/backend/src/routes/login.ts
@@ -8,5 +8,6 @@ const login = new LoginController();
 const loginDataValidate = new LoginDataValidate();
 
 router.post('/', loginDataValidate.validateAtributes, login.validUserController);
+router.get('/validate', loginDataValidate.authToken, login.getValidate);
 
 export default router;
