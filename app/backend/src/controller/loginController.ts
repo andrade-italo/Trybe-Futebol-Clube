ca67fb817a6271ac472592a6e892447ecb1838f4
Italo Andrade <italoandrade13@gmail.com>
2022-07-31 02:22:33 -0300

    fix: statuscode

--- app/backend/src/controller/loginController.ts
+++ app/backend/src/controller/loginController.ts
@@ -13,7 +13,7 @@ class LoginController {
   public validUserController = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const loginResponse = await this.loginService.validUser(email, password);
-    if (loginResponse.message) return res.status(StatusCodes.BAD_REQUEST).json(loginResponse);
+    if (loginResponse.message) return res.status(StatusCodes.UNAUTHORIZED).json(loginResponse);
     return res.status(StatusCodes.OK).json(loginResponse);
   };
 
