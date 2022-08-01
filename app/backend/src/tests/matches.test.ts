b877b3dc28cac486f15336f5e421f6c6cb2de3af
Italo Andrade <italoandrade13@gmail.com>
2022-08-01 19:22:59 -0300

    test: tdd post /matches and validate middleware inside router

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -6,8 +6,11 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import Matches from '../database/models/MatchesModel';
 const MatchesMock = require('./mocks/matchesMock.json')
+const userMock = require('./mocks/userMock.json')
+const MatchesMockCreate = require('./mocks/matchesMockCreate.json')
 
 import { Response } from 'superagent';
+import User from '../database/models/UserModel';
 
 chai.use(chaiHttp);
 
@@ -79,5 +82,66 @@ describe('Testando a rota get /Matches?inProgress=false', () => {
     
     expect(chaiHttpResponse.body).to.be.eql(MatchesMock[1]);
     expect(chaiHttpResponse).to.have.status(200);
+  });
+});
+
+describe('Testando a rota post /matches', async () => {
+
+  before(async () => {
+    sinon
+      .stub(Matches, "create")
+      .resolves(MatchesMockCreate);
+    sinon
+      .stub(User, "findOne")
+      .resolves(userMock[0] as User);
+  });
+
+  after(()=>{
+    (Matches.create as sinon.SinonStub).restore();
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  let chaiHttpResponse: Response;
+
+  it('Ao fazer a requisição para a rota post matches com um body valido e authorização, retorna as partidas criadas e o status 201', async () => {
+    const bodyValido: {} = {
+      email: 'italo@gmail.com',
+      password: '123456'
+    }
+    const getToken = await chai
+    .request(app)
+    .post('/login')
+    .send(bodyValido);
+    const { token } = getToken.body;
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/matches')
+      .set({ authorization: token })
+      .send({
+        "homeTeam": 16,
+        "awayTeam": 8,
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      });
+      
+    expect(chaiHttpResponse.body).to.be.eql(MatchesMockCreate);
+    expect(chaiHttpResponse).to.have.status(201);
+  });
+
+  it('Ao fazer a requisição para a rota post matches sem uma autorização, retorna uma mensagem e o status 401', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/matches')
+      .set({ authorization: 'token' })
+      .send({
+        "homeTeam": 16,
+        "awayTeam": 8,
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      });
+    
+    expect(chaiHttpResponse.body).to.be.eql({ message: "Token must be a valid token" });
+    expect(chaiHttpResponse).to.have.status(401);
   });
 });
